0f0e7ecd72f4a8189aecec5cfe2003768df547f8
Spellz49 <44029351+Spellz49@users.noreply.github.com>
2020-02-11 14:18:28 +0100

    Initial commit

---
+++ Assets/Script/Navigate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Navigate: MonoBehaviour
+    {
+       
+        public void Geography()
+        {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+        }
+
+         public void Biology()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +2);
+        }
+         public void Physics()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +3);
+        }
+           public void Back()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex  -1);
+        }
+        public void close ()
+	    {
+		Application.Quit();
+	    }
+    }
+
